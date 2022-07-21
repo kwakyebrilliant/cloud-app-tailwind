7fa131299ed3d8841f3dbeca509f5b3033fbc151
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 22:15:22 +0000

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className='w-full my-32'>
+        <div className='max-w-[1240px] mx-auto'>
+            <div className='text-center'>
                 <h2>Trusted by developers across the world</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque asperiores earum placeat veritatis dignissimos itaque.</p>
             </div>

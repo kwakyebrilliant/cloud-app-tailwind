73e3957f59b0baa4bd2c5539f9b406932c5f0d70
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 22:04:24 +0000

    some updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,23 @@ const About = () => {
                 <h2>Trusted by developers across the world</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque asperiores earum placeat veritatis dignissimos itaque.</p>
             </div>
+
+            <div>
+                <div>
+                    <p>100%</p>
+                    <p>Completion</p>
+                </div>
+
+                <div>
+                    <p>24/7</p>
+                    <p>Delivery</p>
+                </div>
+
+                <div>
+                    <p>100K</p>
+                    <p>Transactions</p>
+                </div>
+            </div>
         </div>
     </div>
   )

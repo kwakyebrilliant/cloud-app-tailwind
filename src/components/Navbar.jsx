a82eb000322b3f782b1037a4c4322277bd1c8c63
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 18:39:50 +0000

    some commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import { MenuIcon, XIcom } from "@heroicons/react/outline";
+
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+
+        </div>
+        <MenuIcon className='w-5' />
     </div>
   )
 }

415ce06ff0c49e47bc38ceb56b6a5a835afc37e8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 20:43:38 +0000

    some updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { MenuIcon, XIcom } from "@heroicons/react/outline";
 
 const Navbar = () => {
+
+    
+
   return (
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
@@ -24,10 +27,25 @@ const Navbar = () => {
                 <button className='px-8 py-3'>Sign Up</button>
             </div>
 
-            
+            <div className='md:hidden '>
+                <MenuIcon className='w-5' />
+            </div>
 
         </div>
-        <MenuIcon className='w-5' />
+        
+        <ul className='absolute bg-zinc-200 w-full px-8'>
+            <li className='border-b-2 border-zinc-300 w-full'>Home</li>
+            <li className='border-b-2 border-zinc-300 w-full'>About</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Support</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Platforms</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Pricing</li>
+
+            <div className='flex flex-col my-4'>
+            <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
+            <button className='px-8 py-3'>Sign Up</button>
+            </div>
+        </ul>
+        
     </div>
   )
 }

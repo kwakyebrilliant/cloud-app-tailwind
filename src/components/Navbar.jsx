7c429f5255738b9f1b116d531e62273e1282ff8d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 20:05:00 +0000

    ul added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,13 @@ const Navbar = () => {
         <div className='px-2 flex justify-between items-center w-full h-full'>
             <div className='flex items-center'>
                 <h1 className='text-3xl font-bold mr-4 sm:txt-4xl'>CLOUDit.</h1>
+                <ul className='hidden md:flex'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Support</li>
+                    <li>Platforms</li>
+                    <li>Pricing</li>
+                </ul>
             </div>
 
         </div>

bff1fbb461cd2af64b781fb34d4ac49ae3ae0863
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 22:01:04 +0000

    about updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        About
+        <div>
+            <div>
+                <h2>Trusted by developers across the world</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque asperiores earum placeat veritatis dignissimos itaque.</p>
+            </div>
+        </div>
     </div>
   )
 }

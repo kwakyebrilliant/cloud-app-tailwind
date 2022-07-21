0e77c88c10d25aa48dce42840726fb4816ea553f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 20:56:51 +0000

    some updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 
-import { MenuIcon, XIcom } from "@heroicons/react/outline";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
 
-    
+    const [nav, setNav] = useState(false)
+
+    const handleClick = () => setNav(!nav)
+
+    const handleClose =()=> setNav(!nav)
 
   return (
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
@@ -27,13 +31,13 @@ const Navbar = () => {
                 <button className='px-8 py-3'>Sign Up</button>
             </div>
 
-            <div className='md:hidden '>
-                <MenuIcon className='w-5' />
-            </div>
+            <div className='md:hidden mr-4' onClick={handleClick}>
+            {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' />}
+        </div>
 
         </div>
         
-        <ul className='absolute bg-zinc-200 w-full px-8'>
+        <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
             <li className='border-b-2 border-zinc-300 w-full'>Home</li>
             <li className='border-b-2 border-zinc-300 w-full'>About</li>
             <li className='border-b-2 border-zinc-300 w-full'>Support</li>

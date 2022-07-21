54cdbde8d73acc5362871e8de70d29d3e50f3c82
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-21 18:46:37 +0000

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,9 @@ const Navbar = () => {
   return (
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
+            <div className='flex items-center'>
+                <h1 className='text-3xl font-bold mr-4 sm:txt-4xl'>CLOUDit.</h1>
+            </div>
 
         </div>
         <MenuIcon className='w-5' />

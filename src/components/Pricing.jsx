14877c9f199848b00399123a56ade32f9d51bb62
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-22 14:09:38 +0000

    some updates

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -12,17 +12,18 @@ const Pricing = () => {
             <div className='max-w-[1240px] mx-auto py-12'>
                 <div className='text-center py-8 text-slate-300'>
                     <h2 className='text-3xl uppercase'>Pricing</h2>
-                    <h3>The right price for your research.</h3>
-                    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+                    <h3 className='text-5xl font-bold text-white py-8'>The right price for your research.</h3>
+                    <p className='text-3xl'> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
                         laudantium odio ullam inventore aliquid ipsum quasi tenetur velit
                         voluptatum iste.
                     </p>
                 </div>
 
 
-                <div>
-                    <div>
-                       <span>Standard</span>
+                <div className='grid md:grid-cols-2'>
+
+                    <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl'>
+                       <span className='uppercase px-3 py-1 bg-red-300 text-red-900 rounded-2xl text-sm'>Standard</span>
                        <div>
                         <p>$49<span>/mo</span></p>
                        </div>

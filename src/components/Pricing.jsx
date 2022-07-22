d6514fb0be3ebc6094b20f7b707cd9c1bf212262
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-22 14:13:33 +0000

    updates

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -25,16 +25,16 @@ const Pricing = () => {
                     <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl'>
                        <span className='uppercase px-3 py-1 bg-red-300 text-red-900 rounded-2xl text-sm'>Standard</span>
                        <div>
-                        <p>$49<span>/mo</span></p>
+                            <p className='text-6xl font-bold py-4 flex'>$49<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
                        </div>
-                       <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-                       <div>
+                            <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <div className='text-2xl'>
                             <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-700'  />Lorem, ipsum dolor.</p>
                             <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-700'  />Lorem, ipsum dolor.</p>
                             <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-700'  />Lorem, ipsum dolor.</p>
                             <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-700'  />Lorem, ipsum dolor.</p>
                             <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-700'  />Lorem, ipsum dolor.</p>
-                        <button>Get Started</button>
+                            <button className='w-full py-4 my-4'>Get Started</button>
                        </div>
                     </div>
                 </div>

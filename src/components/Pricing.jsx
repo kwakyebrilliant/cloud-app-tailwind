3ab6cd64b49fb5da9baa828329bacc2e7e6506e7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-22 11:46:02 +0000

    some updates

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import { CheckIcon } from '@heroicons/react/solid';
+
 const Pricing = () => {
   return (
     <div>
-        
+        <div>
+
+            <div>Absolute</div>
+
+            <div>
+                <div>
+                    <h2>Pricing</h2>
+                    <h3>The right price for your research.</h3>
+                    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+                        laudantium odio ullam inventore aliquid ipsum quasi tenetur velit
+                        voluptatum iste.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }

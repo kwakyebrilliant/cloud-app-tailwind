5caa73e5d333049ee520645e07351dfc66127876
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-22 16:25:44 +0000

    footer final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,6 @@ const Footer = () => {
   return (
     <div className='w-full mt-24 bg-slate-900 text-gray-300 py-y px-2'>
         <div className='max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8'>
-            
             <div>
                 <h6 className='font-bold uppercase pt-2'>Solutions</h6>
                 <ul>
@@ -23,7 +22,6 @@ const Footer = () => {
                     <li className='py-1'>Cloud</li>
                 </ul>
             </div>
-
             <div>
                 <h6 className='font-bold uppercase pt-2'>Support</h6>
                 <ul>
@@ -33,7 +31,6 @@ const Footer = () => {
                     <li className='py-1'>API Status</li>
                 </ul>
             </div>
-
             <div>
                 <h6 className='font-bold uppercase pt-2'>Company</h6>
                 <ul>
@@ -44,7 +41,6 @@ const Footer = () => {
                     <li className='py-1'>Partners</li>
                 </ul>
             </div>
-
             <div>
                 <h6 className='font-bold uppercase pt-2'>Legal</h6>
                 <ul>
@@ -55,7 +51,6 @@ const Footer = () => {
                     <li className='py-1'>Conditions</li>
                 </ul>
             </div>
-
             <div className='col-span-2 pt-8 md:pt-2'>
                 <p className='font-bold uppercase'>Subscribe to our newsletter</p>
                 <p className='py-4'>The latest news, articles, and resources, sent to your inbox weekly.</p>
@@ -64,10 +59,18 @@ const Footer = () => {
                     <button className='p-2 mb-4'>Subscribe</button>
                 </form>
             </div>
-        
-
         </div>
-        
+
+        <div className='flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500'>
+        <p className='py-4'>2022 Workflow, LLC. All rights reserved</p>
+        <div className='flex justify-between sm:w-[300px] pt-4 text-2xl'>
+            <FaFacebook />
+            <FaInstagram />
+            <FaTwitter />
+            <FaTwitch />
+            <FaGithub />
+        </div>
+        </div>
     </div>
   )
 }

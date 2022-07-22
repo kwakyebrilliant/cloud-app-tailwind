3110988cc6b57cfbee2d69b4ae5c4ba58086aa60
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-22 09:07:14 +0000

    classes updates

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -17,7 +17,7 @@ const Support = () => {
             <div className='px-4 py-12'>
                 <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Support</h2>
                 <h3 className='text-5xl font-bold py-6 text-center'>Finding the right team</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
+                <p className='py-4 text-3xl text-slate-300 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
             </div>
 
             <div>

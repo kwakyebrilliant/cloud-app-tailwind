6a69de78e8c4ed4acf03b861c0ed5a83d68c9d03
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-22 08:56:33 +0000

    more updates

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -13,15 +13,14 @@ const Support = () => {
         <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" />
       </div>
 
-        <div>
-            <div>
+        <div className='max-w-[1240p] mx-auto text-white relative'>
+            <div className='px-4 py-12'>
                 <h2>Support</h2>
                 <h3>Finding the right team</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
             </div>
-        </div>
 
-        <div>
+            <div>
             <div>
                 <div>
                     <PhoneIcon />
@@ -34,6 +33,10 @@ const Support = () => {
             </div>
 
         </div>
+
+        </div>
+
+        
         
     </div>
   )

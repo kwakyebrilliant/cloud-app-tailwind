539849efaf6095450c93a7f0207383b4dcafc710
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-22 16:18:54 +0000

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,14 @@ import {
 
 const Footer = () => {
   return (
-    <div>
+    <div className='w-full mt-24 bg-slate-900 text-gray-300 py-y px-2'>
+        <div className='max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8'>
+            
+            <div>
+
+            </div>
+            
+        </div>
         
     </div>
   )

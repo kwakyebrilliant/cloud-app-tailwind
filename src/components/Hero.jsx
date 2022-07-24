672c42f8f01c6202ddf8ec087f69e8752d2d2a38
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-24 21:26:16 +0000

    hero updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
             border border-slate-300 rounded-xl text-center shadow-xl'>
                 <p>Data Services</p>
-                <div className='flex justify-between flex-wrap px-4'>
+                <div className='flex justify-between flex-col px-8 md:flex-row'>
                     <p className='flex px-4 py-2 text-slate-500'><CloudUploadIcon className='h-6 text-red-700' /> App Security</p>
                     <p className='flex px-4 py-2 text-slate-500'><DatabaseIcon className='h-6 text-red-700' /> Dashboard Design</p>
                     <p className='flex px-4 py-2 text-slate-500'><ServerIcon className='h-6 text-red-700' /> Cloud Data</p>
